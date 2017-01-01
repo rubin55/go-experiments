ddedcffac551a2d9306b42f318b22116c12dd27a
Rubin Simons <rubin@raaftech.com>
2017-01-01 16:06:58 +0100

    A better echo?

--- echo.go
+++ echo.go
@@ -7,9 +7,9 @@ import (
 
 // A comment about the main function.
 func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
 		sep = " "
 	}
 	fmt.Println(s)
