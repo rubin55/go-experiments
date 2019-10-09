b297a9510ca45bd9aa44cff1b9f822b88c8a6fa3
Rubin Simons <rubin@raaf.tech>
2019-10-09 11:35:44 +0200

    This is a go module now.. Experimenting, nothing to see here!

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rubin55/go-experiments
+
+go 1.11
+
+require rsc.io/quote v1.5.2
--- hello.go
+++ hello.go
@@ -1,7 +1,7 @@
-package main
+package hello
 
-import "fmt"
+import "rsc.io/quote"
 
-func main() {
-	fmt.Println("Hello, world!")
+func Hello() string {
+	return quote.Hello()
 }
