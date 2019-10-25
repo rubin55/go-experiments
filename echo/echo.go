c15842a09ca9c137d7819f6f0a181d7476a90548
Rubin Simons <rubin@raaf.tech>
2019-10-25 21:51:38 +0200

    Getting on with modules in Go 1.11+

---
+++ echo/echo.go
@@ -0,0 +1,15 @@
+package echo
+
+import (
+	"fmt"
+	"os"
+)
+
+func Echo() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
---
+++ main/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/rubin55/go-experiments/echo"
+)
+
+func main() {
+	echo.Echo()
+}
