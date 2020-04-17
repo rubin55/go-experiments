3106259fca31fe44a85785ab3eb1b01d63bd113a
Rubin Simons <rubin@raaf.tech>
2020-04-17 21:30:45 +0200

    Added RabbitMQ publisher/consumer example in Go.

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/rubin55/go-experiments
 
 go 1.11
 
-require rsc.io/quote v1.5.2
+require github.com/streadway/amqp v0.0.0-20200108173154-1c71cc93ed71
--- main/main.go
+++ main/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"github.com/rubin55/go-experiments/echo"
-	"github.com/rubin55/go-experiments/hello"
-	"github.com/rubin55/go-experiments/unique"
-	"os"
+	"github.com/rubin55/go-experiments/rabbit"
+	"time"
 )
 
 func main() {
-	fmt.Println(hello.Hello() + " " + echo.Echo(os.Args))
-	fmt.Println(unique.NewUnique(os.Args[1:]))
+	rabbit.Publish()
+	time.Sleep(2 * time.Second)
+	rabbit.Consume()
 }
